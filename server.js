1bcb1540901e1c5999428c46cb2fc6478eccb5cd
shubhambuwa440-coder <shubhambuwa440@gmail.com>
2026-03-16 22:31:02 +0530

    Initialize package.json for PDF toolset project

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "pdfflow-modern",
+  "version": "1.0.0",
+  "description": "Modern PDF Toolset",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.18.2",
+    "multer": "^1.4.5-lts.1",
+    "pdf-lib": "^1.17.1",
+    "fs-extra": "^11.1.1"
+  }
+}
